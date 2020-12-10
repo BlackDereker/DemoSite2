c547294befe1239956fc534e293e2c2bb5d4082a
Bruno Godoi Machado <bruno.machado@mutantbr.com>
2020-12-10 04:42:08 -0300

    Converted jar to war

--- src/admin/src/main/java/com/community/admin/AdminApplication.java
+++ src/admin/src/main/java/com/community/admin/AdminApplication.java
@@ -5,14 +5,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.broadleafcommerce.common.web.boot.support.BroadleafBootServletContextInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class AdminApplication {
+public class AdminApplication extends BroadleafBootServletContextInitializer {
 
     @Configuration
     @EnableBroadleafAdminAutoConfiguration
     public static class BroadleafFrameworkConfiguration {}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AdminApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
